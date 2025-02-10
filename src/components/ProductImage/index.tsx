2c3346571fad26b015d1056b6d15c3b2c3d3dd67
art-dev-litvin <artem.litvin.dev@gmail.com>
2025-02-10 18:25:41 +0200

    extract productImage component

---
+++ src/components/ProductImage/index.tsx
@@ -0,0 +1,39 @@
+import { ProductStatus } from "@app/types/productFields";
+import Image, { ImageProps } from "next/image";
+import React from "react";
+
+export interface ProductImageProps extends ImageProps {
+  productStatus: ProductStatus;
+  wrapperClassname?: string;
+  statusClassname?: string;
+}
+
+export default function ProductImage({
+  src,
+  alt,
+  productStatus,
+  wrapperClassname,
+  statusClassname,
+  ...props
+}: ProductImageProps) {
+  const isOutOfStock = productStatus === ProductStatus.OutOfStack;
+
+  return (
+    <div
+      className={`basis-[500px] h-[700px] relative bg-white rounded-xl overflow-hidden ${wrapperClassname}`}>
+      <Image
+        fill
+        src={src}
+        alt={alt}
+        className={`mb-4 object-cover ${isOutOfStock ? "opacity-35" : "opacity-100"}`}
+        {...props}
+      />
+      {isOutOfStock && (
+        <p
+          className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-black ${statusClassname}`}>
+          {productStatus}
+        </p>
+      )}
+    </div>
+  );
+}
