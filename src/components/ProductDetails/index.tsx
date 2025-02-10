11d7775d9c7ceee101f84338e6395473d0595599
art-dev-litvin <artem.litvin.dev@gmail.com>
2025-02-10 18:34:40 +0200

    improve details page styles

--- src/components/ProductDetails/index.tsx
+++ src/components/ProductDetails/index.tsx
@@ -23,32 +23,37 @@ export default function ProductDetails({ productId }: ProductDetailsProps) {
     <div className="container mx-auto  flex gap-10 pt-12 text-white">
       <ProductImage
         className="basis-[500px] h-[700px]"
+        statusClassname="text-3xl"
         src={product.imageSrc}
         alt={product.name}
         productStatus={product.status}
       />
-      <div className="grow text-lg relative">
-        <h1 className="text-4xl font-bold mb-6">{product.name}</h1>
-        <Price className="text-2xl" unitClassName="text-base" product={product} />
-        <p className="mb-2">{product.description}</p>
-        <p className="text-white mb-2">
-          <span className="font-bold">Category:</span> {product.category}
-        </p>
+      <div className="grow flex flex-col text-lg relative">
+        <div className="grow">
+          <h1 className="text-4xl font-bold mb-6">{product.name}</h1>
+          <Price className="text-2xl" unitClassName="text-base" product={product} />
+          <p className="mb-2">{product.description}</p>
+          <p className="text-white mb-2">
+            <span className="font-bold">Category:</span> {product.category}
+          </p>
 
-        {product.ingredients && (
-          <>
-            <p className="mt-4">Ingredients:</p>
-            <ul className="list-disc list-inside">
-              {product.ingredients.map((ingredient, index) => (
-                <li key={index}>{ingredient}</li>
-              ))}
-            </ul>
-          </>
-        )}
+          {product.ingredients && (
+            <>
+              <p className="mt-4">Ingredients:</p>
+              <ul className="list-disc list-inside">
+                {product.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
 
-        <Button className="mt-3" href={`/products/${product.id}/edit`}>
-          Edit Product
-        </Button>
+        <div className="flex">
+          <Button className="mt-3" href={`/products/${product.id}/edit`}>
+            Edit Product
+          </Button>
+        </div>
       </div>
     </div>
   );
