dbad1eaa1393529efd9d0375ced3965bc5b21cd2
art-dev-litvin <artem.litvin.dev@gmail.com>
2025-02-10 16:14:56 +0200

    configure redux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import ReduxProvider from "@app/components/ReduxProvider";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
     </html>
   );
 }
